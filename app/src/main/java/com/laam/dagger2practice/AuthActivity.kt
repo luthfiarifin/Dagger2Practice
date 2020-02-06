e60287db7d7b651f034ea158032adbd33781058b
luthfi <luthfiarifin0222@gmail.com>
2020-02-06 21:01:32 +0700

    Referencing multiple dagger dependencies within a single module

--- app/src/main/java/com/laam/dagger2practice/AuthActivity.kt
+++ app/src/main/java/com/laam/dagger2practice/AuthActivity.kt
@@ -1,8 +1,11 @@
 package com.laam.dagger2practice
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_auth.*
 import javax.inject.Inject
 
 class AuthActivity : DaggerAppCompatActivity() {
@@ -10,17 +13,22 @@ class AuthActivity : DaggerAppCompatActivity() {
     private val TAG = "AuthActivity"
 
     @Inject
-    lateinit var asdad: String
+    lateinit var logo: Drawable
 
-    @JvmField
     @Inject
-    var isNull: Boolean = true
+    lateinit var requestManager: RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        Log.d(TAG, " $asdad")
-        Log.d(TAG, "is null ? $isNull")
+
+        setLogo()
+    }
+
+    private fun setLogo() {
+        requestManager
+            .load(logo)
+            .into(login_logo)
     }
 }
--- app/src/main/java/com/laam/dagger2practice/di/AppModule.kt
+++ app/src/main/java/com/laam/dagger2practice/di/AppModule.kt
@@ -1,6 +1,12 @@
 package com.laam.dagger2practice.di
 
 import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import com.laam.dagger2practice.R
 import dagger.Module
 import dagger.Provides
 
@@ -9,9 +15,21 @@ object AppModule {
 
     @JvmStatic
     @Provides
-    fun someString(): String = "this is some string"
+    fun provideRequestOption(): RequestOptions = RequestOptions
+        .placeholderOf(R.drawable.white_background)
+        .error(R.drawable.white_background)
 
     @JvmStatic
     @Provides
-    fun getApp(application: Application?): Boolean = application == null
+    fun provideRequestManager(
+        application: Application,
+        requestOptions: RequestOptions
+    ): RequestManager = Glide
+        .with(application)
+        .setDefaultRequestOptions(requestOptions)
+
+    @JvmStatic
+    @Provides
+    fun provideAppDrawable(application: Application): Drawable =
+        ContextCompat.getDrawable(application, R.drawable.codingwithmitch)!!
 }
