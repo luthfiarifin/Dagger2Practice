7180bda0db622c65bc31e8e81cc626d14e18eb53
luthfi <luthfiarifin0222@gmail.com>
2020-02-08 10:59:49 +0700

    Injection view mmodel

--- app/src/main/java/com/laam/dagger2practice/viewmodels/ViewModelProviderFactory.kt
+++ app/src/main/java/com/laam/dagger2practice/viewmodels/ViewModelProviderFactory.kt
@@ -5,35 +5,6 @@ import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 import javax.inject.Provider
 
-//
-//class ViewModelProviderFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, Provider<ViewModel>>) :
-//    ViewModelProvider.Factory {
-//    private val TAG = "ViewModelProviderFactory"
-//
-//    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-//        var creator: Provider<out ViewModel?>? = creators[modelClass]
-//
-//        creator?.let {
-//            for ((key, value) in creators.entries) { // if it's allowed, set the Provider<ViewModel>
-//                if (modelClass.isAssignableFrom(key)) {
-//                    creator = value
-//                    break
-//                }
-//            }
-//        }.run {
-//            throw IllegalArgumentException("unknown model class $modelClass")
-//        }
-//
-//        @Suppress("UNREACHABLE_CODE", "UNCHECKED_CAST")
-//        try {
-//            return creator!!.get() as T
-//        } catch (ex: Exception) {
-//            @Suppress("ThrowableNotThrown")
-//            throw RuntimeException(ex)
-//        }
-//    }
-//}
-
 class ViewModelProviderFactory @Inject constructor(private val creators: Map<Class<out ViewModel?>?, @JvmSuppressWildcards Provider<ViewModel>?>) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
